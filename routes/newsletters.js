e6e67d2144ac3a44f66b70a829a2046a9c030135
noman <naumanashraf30@gmail.com>
2022-02-08 20:03:25 +0500

    ddd

---
+++ routes/newsletters.js
@@ -0,0 +1,31 @@
+const { default: axios } = require("axios");
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  const { data } = await axios.get(
+    "https://collections.rarity.tools/upcoming2"
+  );
+  const newdata = data.map((val) => {
+    delete Object.assign(val, { ["imagecount"]: val["Image Count"] })[
+      "Image Count"
+    ];
+    delete Object.assign(val, { ["description"]: val["Short Description"] })[
+      "Short Description"
+    ];
+    delete Object.assign(val, { ["presaleDate"]: val["Listed Date"] })[
+      "Listed Date"
+    ];
+    delete Object.assign(val, { ["saleDate"]: val["Sale Date"] })["Sale Date"];
+    delete Object.assign(val, { ["maxItems"]: val["Max Items"] })["Max Items"];
+
+    return val;
+  });
+  console.log(newdata);
+  res.status(200).json({
+    newdata,
+  });
+});
+
+module.exports = router;
